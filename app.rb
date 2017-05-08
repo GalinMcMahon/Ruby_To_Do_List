027778a88bc1a73f0b48b313a6d18c4e30ccb545
Galin McMahon <galinmcmahon@gmail.com>
2017-05-08 10:22:01 -0700

    add Active Record to to_do list

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
 require("sinatra")
 require("sinatra/reloader")
+require("sinatra/activerecord")
 also_reload("lib/**/*.rb")
 require("./lib/task")
 require("./lib/list")
 require("pg")
-
-DB = PG.connect({:dbname => "to_do"})
 
 get("/") do
   erb(:index)
@@ -13,22 +12,25 @@ end
 
 get('/lists') do
   @lists = List.all()
+  @tasks = Task.all()
   erb(:lists)
 end
 
 get("/lists/new") do
+  @tasks = Task.all()
   erb(:list_form)
 end
 
 post("/lists") do
   name = params.fetch("name")
-  list = List.new({:name => name, :id => nil})
-  list.save()
+  list = List.create({:name => name, :id => nil})
+  @tasks = Task.all()
   erb(:list_success)
 end
 
 get("/lists/:id") do
   @list = List.find(params.fetch("id").to_i())
+  @tasks = Task.all()
   erb(:list)
 end
 
@@ -36,7 +38,19 @@ post("/tasks") do
   description = params.fetch("description")
   list_id = params.fetch("list_id").to_i()
   @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save()
+  @task = Task.create({:description => description, :list_id => list_id, :done => false})
   erb(:task_success)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
 end
